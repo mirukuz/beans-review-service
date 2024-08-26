92762b7aeed5d7f54b1617b53201682e48483034
zxc <xinczhou@gmail.com>
2024-08-26 22:27:57 +1000

    update build script

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,9 +4,13 @@ import './review'
 import './roaster'
 import './bean'
 import { writeFileSync } from 'fs'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { printSchema } from 'graphql'
 
 export const schema = builder.toSchema({})
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 writeFileSync(resolve(__dirname, '../../schema.graphql'), printSchema(schema))
